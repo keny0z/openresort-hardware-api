35cfbdc54e62881d1bc02f85502f5ae294690e93
keny0z <kevinospinacdn@gmail.com>
2022-10-21 12:44:16 -0500

    proyecto inicial

---
+++ src/main/java/co/edu/uco/openresort/hardware_api/HardwareApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.openresort.hardware_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uco/openresort/hardware_api/HardwareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.openresort.hardware_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
