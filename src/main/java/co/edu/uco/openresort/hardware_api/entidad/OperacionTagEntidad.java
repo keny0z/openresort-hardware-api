3801ba66cf791b11b3870573271ff6dea966b475
keny0z <kevinospinacdn@gmail.com>
2022-11-22 20:31:31 -0500

    conexion a MongoDB e insercion de documentos funcional

--- src/main/java/co/edu/uco/openresort/hardware_api/entidad/OperacionTagEntidad.java
+++ src/main/java/co/edu/uco/openresort/hardware_api/entidad/OperacionTagEntidad.java
@@ -2,7 +2,6 @@ package co.edu.uco.openresort.hardware_api.entidad;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -11,8 +10,7 @@ import java.time.LocalDateTime;
 @Setter
 @Document(collection = "operacion_tag")
 public class OperacionTagEntidad {
-    @Id
-    private long id;
+
     private long tag;
     private LocalDateTime fecha;
     private String operacion;
--- src/main/java/co/edu/uco/openresort/hardware_api/servicio/implementacion/AccesoHabitacionServicioImplementacion.java
+++ src/main/java/co/edu/uco/openresort/hardware_api/servicio/implementacion/AccesoHabitacionServicioImplementacion.java
@@ -1,20 +1,36 @@
 package co.edu.uco.openresort.hardware_api.servicio.implementacion;
 
 import co.edu.uco.openresort.hardware_api.cliente.HttpCliente;
+import co.edu.uco.openresort.hardware_api.entidad.OperacionTagEntidad;
+import co.edu.uco.openresort.hardware_api.repositorio.OperacionTagRepositorio;
 import co.edu.uco.openresort.hardware_api.servicio.AccesoHabitacionServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class AccesoHabitacionServicioImplementacion implements AccesoHabitacionServicio {
 
     @Autowired
     private HttpCliente httpCliente;
+    @Autowired
+    private OperacionTagRepositorio operacionTagRepositorio;
 
     @Override
     public ResponseEntity<?> darAcceso(long idTag, int idHabitacion) throws Exception {
+        OperacionTagEntidad operacionTagEntidad = new OperacionTagEntidad();
+
+        operacionTagEntidad.setTag(idTag);
+        operacionTagEntidad.setFecha(LocalDateTime.now());
+        operacionTagEntidad.setOperacion("acceso a la habitacion con id: "+ idHabitacion);
+
+        operacionTagRepositorio.insert(operacionTagEntidad);
+
         return httpCliente.post("http://localhost:8080/habtacion/acceso","{\"idTag\":" + idTag + ",\"idHabitacion\":" + idHabitacion + "}");
+
+
     }
 
     @Override
