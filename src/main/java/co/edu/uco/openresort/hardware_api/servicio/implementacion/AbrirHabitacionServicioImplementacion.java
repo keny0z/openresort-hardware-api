8d56847205b316735ec87e80b152cbd236172e71
keny0z <kevinospinacdn@gmail.com>
2022-11-23 23:08:42 -0500

    insertando operacion en mongo al momento de abrir puerta

--- src/main/java/co/edu/uco/openresort/hardware_api/servicio/implementacion/AbrirHabitacionServicioImplementacion.java
+++ src/main/java/co/edu/uco/openresort/hardware_api/servicio/implementacion/AbrirHabitacionServicioImplementacion.java
@@ -1,20 +1,40 @@
 package co.edu.uco.openresort.hardware_api.servicio.implementacion;
 
 import co.edu.uco.openresort.hardware_api.cliente.HttpCliente;
+import co.edu.uco.openresort.hardware_api.entidad.OperacionTagEntidad;
+import co.edu.uco.openresort.hardware_api.repositorio.OperacionTagRepositorio;
 import co.edu.uco.openresort.hardware_api.servicio.AbrirHabitacionServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class AbrirHabitacionServicioImplementacion implements AbrirHabitacionServicio {
 
     @Autowired
     private HttpCliente httpCliente;
+    @Autowired
+    private OperacionTagRepositorio operacionTagRepositorio;
 
 
     @Override
     public ResponseEntity<?> abrir(long idTag, int idHabitacion) throws Exception {
-        return httpCliente.post("http://localhost:8080/habtacion/abrir","{\"idTag\":" + idTag + ",\"idHabitacion\":" + idHabitacion + "}");
+        OperacionTagEntidad operacionTagEntidad = new OperacionTagEntidad();
+
+        operacionTagEntidad.setTag(idTag);
+        operacionTagEntidad.setFecha(LocalDateTime.now());
+        operacionTagEntidad.setOperacion("ingreso a la habitacion con id: "+ idHabitacion);
+
+
+        ResponseEntity respuesta = httpCliente.post("http://localhost:8080/habtacion/abrir","{\"idTag\":" + idTag + ",\"idHabitacion\":" + idHabitacion + "}");
+
+        //La comparacion debe ser por status code y no por body
+        if (respuesta.getBody().toString().equals("true")){
+            operacionTagRepositorio.insert(operacionTagEntidad);
+        }
+
+        return respuesta;
     }
 }
